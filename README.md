d47af22b3ef4c8443a61acbcc2e5930152ad96b5
Ralph Potztal <dennerlager@gmail.com>
2026-08-21 19:21:37 +0200

    handle inverted bits in parser

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # py-wahoo-kickr-core-2
 python driver for wahoo kickr core 2
+
+this software is based on:\
+https://github.com/elfrances/wahoo-fitness-tnp/tree/main/\
+https://github.com/kilianyp/hacs-wahoo-wftnp\
--- parser.py
+++ parser.py
@@ -16,9 +16,8 @@ def parse(characteristic, payload):
 class Parser:
     def parse(self, payload):
         flags = self.flagsFormat.unpack_from(payload, 0)[0]
-        # set bit 0 to indicate that instantaneous speed is sent,
-        # because it is
-        flags |= 1
+        # some bits are active-low. invert them that all are active-high:
+        flags ^= self.inverterMask
         offset = self.flagsFormat.size
         data = {}
         for bit, field in self.bits.items():
@@ -28,9 +27,8 @@ class Parser:
         return data
 
 class IndoorBikeDataParser(Parser):
-    """instantaneous speed is present even if bit is not set.
-    double check bit 8"""
     flagsFormat = struct.Struct('<H')
+    inverterMask = 1
     bits = {
         0: Field(name='instantaneous speed', format_='<H', unit='km/h', scale=0.01),
         1: Field(name='average speed', format_='<H', unit='km/h', scale=0.01),
@@ -40,7 +38,7 @@ class IndoorBikeDataParser(Parser):
         5: Field(name='resistance level', format_='<B'),
         6: Field(name='instantaneous power', format_='<h', unit='W'),
         7: Field(name='average power', format_='<h', unit='W'),
-        8: Field(name='expended energy', format_='<H', unit='kcal'),
+        8: Field(name='expended energy', format_='<HHB', unit='kcal'),
         9: Field(name='heart rate', format_='<B', unit='bpm'),
         10: Field(name='metabolic equivalent', format_='<B'),
         11: Field(name='elapsed time', format_='<H', unit='s'),
@@ -49,5 +47,4 @@ class IndoorBikeDataParser(Parser):
 
 parser = {
     uuid.UUID('00002ad2-0000-1000-8000-00805f9b34fb'): IndoorBikeDataParser,
-    uuid.UUID('00002ada-0000-1000-8000-00805f9b34fb'): FitnessMachineStatusParser,
     }
